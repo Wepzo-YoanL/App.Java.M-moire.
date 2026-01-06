e2ee76fdd81e755685f50313b9ebbf5452ee4970
YoanL <levyyohann01@gmail.com>
2026-01-06 07:53:16 +0100

    Utilisation des conditions avec un tableau de chaînes caractères String

--- src/App/Java02/Main.java
+++ src/App/Java02/Main.java
@@ -6,11 +6,16 @@ public class Main {
 		// TODO Auto-generated method stub
 	// Mise en place d'un tableau
 	// Les tableaux sont réalisés avec un String pour les chaînes de caractère avec l'ajout de []
-		String[] names = {"igor, Mathieu, Jean Luc"};
+		String[] names = {"igor"," Mathieu", "Jean Luc"};
 		//Au dessus nous avons élaboré une case avec un élément qui est tout ce qui est contenu dans la même paire 
 		// De griffe
 		System.out.println(names[0]);
-
+			// Ici nous avons la comparaison de deux éléments du tableau 
+if (names[0]== names[1]) {
+	System.out.println("C'est la meme chose");
+}else {
+	System.out.println("C'est pas la meme chose");
+}
 	}
 
 }
