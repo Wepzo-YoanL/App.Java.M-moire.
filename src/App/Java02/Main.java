a8c17b3400cebd8236b0e8d62aae7bbcd475ac92
YoanL <levyyohann01@gmail.com>
2026-01-06 07:44:15 +0100

    Morceau de code première utilisation d'un tableau String

--- src/App/Java02/Main.java
+++ src/App/Java02/Main.java
@@ -4,23 +4,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//Soit la mise en place du variable int money avec pour n'importe quel valeur
-	
-	
-		int money = 1000;
-		// if la variable de condition 
-		// Qui dit que si la moneu équivaut à 2000
-	
-		if(money == 2000) {
-			System.out.print("La somme est suffisante pour se payer le téléphone");
-			//Le system renvoit le message la somme est okay
-			// else pour signifier aussi
-			// Si la somme atendue n'est plus celle demandée
-			// le second message est reournée
-		} else {	
-			//Celui-i est le second message retournée
-			System.out.println("La somme n'est pas suffisante");
-		}
+	// Mise en place d'un tableau
+	// Les tableaux sont réalisés avec un String pour les chaînes de caractère avec l'ajout de []
+		String[] names = {"igor, Mathieu, Jean Luc"};
+		//Au dessus nous avons élaboré une case avec un élément qui est tout ce qui est contenu dans la même paire 
+		// De griffe
+		System.out.println(names[0]);
 
 	}
 
