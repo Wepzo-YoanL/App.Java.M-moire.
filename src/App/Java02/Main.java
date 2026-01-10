f86f941115f5bdfe016ea3cc6171f0e08b168e01
YoanL <levyyohann01@gmail.com>
2026-01-10 23:05:33 +0100

    Utilisation de la Boucle "for"

--- src/App/Java02/Main.java
+++ src/App/Java02/Main.java
@@ -4,15 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	//Tableau composé de plusieurs tableau 
-		//Pour se faire de paires de croché
-	String Xspeudos = "Java, Python, Html, JavaScript, Php, C++, GdScirpt";
-	// La syntaxe "split" permet de marquer un symbole comme limite
-	// Ici la virgule sera prise comme limite pour tous les names 
-	String Speudo[]	= Xspeudos.split(",");
-		//Erreur résolu
+	for(int i=0; i<100; i++) {
+		System.out.println("Le nombre de Boucle faite est:" +" " + i);
+	}
 	
-			System.out.println("Voici le nombre d'éléments:" + "  " + Speudo.length);
+		
 
 	}
 
