8ebffc5828c534174108f924d7e89b9cada3d318
YoanL <levyyohann01@gmail.com>
2026-01-10 22:47:11 +0100

    Erreur.Utilisation de la syntaxe "split"

--- src/App/Java02/Main.java
+++ src/App/Java02/Main.java
@@ -6,21 +6,11 @@ public class Main {
 		// TODO Auto-generated method stub
 	//Tableau composé de plusieurs tableau 
 		//Pour se faire de paires de croché
-	int [][] numbers = {
-			//Premier tableau
-			{
-			1, 2, 3	
-			},
-			//Deuxieme tableau
-			{
-			4, 5, 6	
-			},
-			// Troisieme tableau
-			{
-				7, 8, 9
-			}
-			} ;
-			System.out.println(" " + numbers[2][0] +" " + "Est le chiffre recherché");
+	String Xspeudos = "Java, Python, Html, JavaScript, Php, C++, GdScirpt";
+	String Speudo[]	= Xspeudos.split(",");
+		//Erreur déclenché
+		// Cause Utilisation du name "Xspeudos" pour faire l'appel du nombre de Xspeudos
+			System.out.println(Xpeudos.length);
 
 	}
 
