0fef7c1d8178fe98ec8327828fa537a1c49e1822
YoanL <levyyohann01@gmail.com>
2026-01-10 22:34:11 +0100

    Utilisation des tableaux imbriqués (Nested array)

--- src/App/Java02/Main.java
+++ src/App/Java02/Main.java
@@ -4,14 +4,23 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	// Mise en place d'un tableau
-	// Les tableaux sont réalisés avec un int pour les nombres avec l'ajout de []
-		int[] numbers = {12,25,50,100};
-	int calcul 		  = (numbers[0]+numbers[1]+numbers[2]+numbers[3]) / numbers.length;
-	// le cross "Length est une syntaxe qui permet de donner le nombre de la chose
-	// Donc si je mets length près de numbers ca va me donner le nombre d'éléments numbers
-		System.out.println(numbers.length);
-		System.out.println(calcul);
+	//Tableau composé de plusieurs tableau 
+		//Pour se faire de paires de croché
+	int [][] numbers = {
+			//Premier tableau
+			{
+			1, 2, 3	
+			},
+			//Deuxieme tableau
+			{
+			4, 5, 6	
+			},
+			// Troisieme tableau
+			{
+				7, 8, 9
+			}
+			} ;
+			System.out.println(" " + numbers[2][0] +" " + "Est le chiffre recherché");
 
 	}
 
